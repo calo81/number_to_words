6731263d19ed1139e0abcfa5ec0b18ab8ba5c96f
Carlo <carlo.scarioni@gmail.com>
2012-07-26 00:52:15 +0100

    Changed test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,45 +2,46 @@ require "rspec"
 require "../lib/numbers_to_words"
 
 describe Cacique::NumbersToWords do
+  describe "In english" do
+    before(:each) do
+      Cacique::NumbersToWords.init
+    end
 
-  before(:each) do
-    Cacique::NumbersToWords.init
-  end
+    it "should convert 1" do
+      1.in_words.should == 'one'
+    end
 
-  it "should convert 1" do
-    1.in_words.should == 'one'
-  end
-
-  it "should convert 2" do
+    it "should convert 2" do
       2.in_words.should == 'two'
-  end
+    end
 
 
-  it "should convert 44" do
+    it "should convert 44" do
       44.in_words.should == 'forty four'
-  end
+    end
 
-  it "should convert 142" do
+    it "should convert 142" do
       142.in_words.should == 'one hundred forty two'
-  end
+    end
 
-  it "should convert 1142" do
+    it "should convert 1142" do
       1142.in_words.should == 'one thousand one hundred forty two'
-  end
+    end
 
-  it "should convert 12" do
+    it "should convert 12" do
       12.in_words.should == 'twelve'
-  end
+    end
 
-  it "should convert 1_234_153" do
-    1_234_153.in_words.should == 'one million two hundred thirty four thousand one hundred fifty three'
-  end
+    it "should convert 1_234_153" do
+      1_234_153.in_words.should == 'one million two hundred thirty four thousand one hundred fifty three'
+    end
 
-  it "should convert 0" do
+    it "should convert 0" do
       0.in_words.should == 'zero'
-  end
+    end
 
-  it "should convert 70" do
+    it "should convert 70" do
       70.in_words.should == 'seventy'
+    end
   end
 end
